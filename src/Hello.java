49b1f897e8caf222f6316e9d4f3c117190391836
Жанна <Janny.Gorelova@gmail.com>
2017-06-21 00:23:27 +0300

    Добавление классов FriendsAndFamily и Hello

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Жанна on 20.06.2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("*    * ***** *     *      **** ");
+        System.out.println("*    * *     *     *     *    * ");
+        System.out.println("****** ***** *     *     *    * ");
+        System.out.println("*    * *     *     *     *    * ");
+        System.out.println("*    * ***** ***** *****  **** ");
+    }
+}
