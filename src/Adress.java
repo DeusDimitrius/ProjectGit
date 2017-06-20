89f578c588cd6a4ded45f3fd2d3b5e3a0736b94c
Жанна <Janny.Gorelova@gmail.com>
2017-06-20 21:16:06 +0300

    Добавление класса Adress

---
+++ src/Adress.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Жанна on 20.06.2017.
+ */
+public class Adress {
+    public static void main(String[] args){
+        System.out.println("Дмитрий Балашов");
+        System.out.println("пр.Пархоменко 35/7");
+        System.out.println("Санкт-Петербург, 194021");
+        System.out.println("Россия");
+    }
+}
